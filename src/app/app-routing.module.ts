9e5d7f7cf143c6f30b370544087fd307bf7714e1
Mykhailo Tovtyn <mykhailo_tovtyn@epam.com>
2022-09-20 17:32:19 +0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AdvantagesComponent } from './advantages/advantages.component';
+import { BracesComponent } from './braces/braces.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { RulesComponent } from './rules/rules.component';
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {path: '', component: NavbarComponent},
+  {path: 'advantages', component: AdvantagesComponent},
+  {path: 'braces', component: BracesComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'rules', component: RulesComponent},
+  {path: 'about', component: AboutComponent}
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {IvyCarouselModule} from 'angular-responsive-carousel';
+import "@ptkdev/webcomponent-instagram-widget";
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AdvantagesComponent } from './advantages/advantages.component';
+import { BracesComponent } from './braces/braces.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { RulesComponent } from './rules/rules.component';
+import { AboutComponent } from './about/about.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    AdvantagesComponent,
+    BracesComponent,
+    GalleryComponent,
+    RulesComponent,
+    AboutComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IvyCarouselModule, 
+    FormsModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class AppModule { }
